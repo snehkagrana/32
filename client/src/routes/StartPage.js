fda599c9f91326815f4b1211ddc7af5b16f01c65
Sneh Kagrana <snehkagrana10@gmail.com>
2023-12-02 22:55:27 +0530

    minor tweaks

--- client/src/routes/StartPage.js
+++ client/src/routes/StartPage.js
@@ -46,7 +46,7 @@ const NextArrow = (props) => {
         <div className={className} style={{ ...style, right: '5px', zIndex: '1' }} onClick={onClick}>
             <FontAwesomeIcon
                 icon={faArrowRight}
-                style={{ fontSize: '24px', color: '#28a745' }}  // Set the color to green (#28a745)
+                style={{ fontSize: '24px', color: '#28a745' }}  
             />
         </div>
     );
@@ -58,7 +58,7 @@ const PrevArrow = (props) => {
         <div className={className} style={{ ...style, left: '5px', zIndex: '1' }} onClick={onClick}>
             <FontAwesomeIcon
                 icon={faArrowLeft}
-                style={{ fontSize: '24px', color: '#28a745' }}  // Set the color to green (#28a745)
+                style={{ fontSize: '24px', color: '#28a745' }}  
             />
         </div>
     );
@@ -164,7 +164,7 @@ const StartPage = () => {
                                         />
                                     </MDBContainer>
                                     <div className="flex justify-between w-full">
-                                        <span className={`flex items-center justify-end lg:text-lg text-sm font-bold whitespace-nowrap flex-1 mr-4 ${darkMode ? 'text-gray-100' : 'text-gray-950'} cursor-pointer`} onClick={() => navigate(`/contactus`)}>Contact Us</span>
+                                        <span className={`flex items-center justify-end lg:text-lg text-sm font-bold whitespace-nowrap flex-1 mr-4 ${darkMode ? 'text-gray-500' : 'text-gray-950'} cursor-pointer`} onClick={() => navigate(`/contactus`)}>Contact Us</span>
                                         <Button
                                             style={{
 
@@ -200,7 +200,7 @@ const StartPage = () => {
                             </h1>
 
                             <div className="flex items-center justify-center">
-                                <h1 className={`scroll-m-20 text-xl font-extrabold tracking-tight lg:text-4xl py-4 ${darkMode ? 'text-gray-100' : 'text-gray-950'}`}>say goodbye to boring finance lessons.</h1>
+                                <h1 className={`scroll-m-20 text-xl font-extrabold tracking-tight lg:text-4xl py-4 ${darkMode ? 'text-gray-500' : 'text-gray-950'}`}>say goodbye to boring finance lessons.</h1>
                             </div>
 
                             
@@ -210,7 +210,7 @@ const StartPage = () => {
                             <div className="flex items-center justify-center max-w-md px-3 mx-auto space-x-3 py-8">
                                 <div className="flex flex-col" style={{ width: '50%' }}>
                                     <h6
-                                        className={`${darkMode ? 'text-gray-100' : 'text-gray-500'} text-center py-1`}
+                                        className={`${darkMode ? 'text-gray-500' : 'text-gray-500'} text-center py-1`}
                                     >
                                         (No signup required.)
                                     </h6>
@@ -282,12 +282,12 @@ const StartPage = () => {
                                     </div>
                                     <div className="flex-col items-start justify-center flex-1 hidden mx-auto md:flex sm:py-10 lg:py-0 ">
                                         <h2 className="text-3xl font-bold text-[#2cb74c]">short, easy-to-read chapters</h2>
-                                        <h3 className={`my-6 text-xl font-bold w-96 ${darkMode ? 'text-gray-100' : 'text-gray-950'}`}>choose from over 600 chapters, each only 3-4 minutes long</h3>
+                                        <h3 className={`my-6 text-xl font-bold w-96 ${darkMode ? 'text-gray-500' : 'text-gray-950'}`}>choose from over 600 chapters, each only 3-4 minutes long</h3>
 
                                     </div>
                                     <div className="flex flex-col items-center justify-center mx-auto mt-5 lg:py-0 md:hidden">
                                     <h2 className="text-3xl font-bold text-[#2cb74c] w-60">short, easy-to-read chapters</h2>
-                                    <h3 className={`my-6 text-base md:text-xl font-bold w-60 ${darkMode ? 'text-gray-100' : 'text-gray-950 w-60'}`}>choose from over 600 chapters, each only 3-4 minutes long</h3>
+                                    <h3 className={`my-6 text-base md:text-xl font-bold w-60 ${darkMode ? 'text-gray-500' : 'text-gray-950 w-60'}`}>choose from over 600 chapters, each only 3-4 minutes long</h3>
 
                                     </div>
 
@@ -313,7 +313,7 @@ const StartPage = () => {
 
                                                 />
                                             </div>
-                                            <h3 className={`-mt-20 text-xl font-bold w-96 ${darkMode ? 'text-gray-100' : 'text-gray-950'}`}>
+                                            <h3 className={`-mt-20 text-xl font-bold w-96 ${darkMode ? 'text-gray-500' : 'text-gray-950'}`}>
                                                 complete quizzes, earn XP points, and increase your daily streak
                                             </h3>
                                         </div>
@@ -323,7 +323,7 @@ const StartPage = () => {
 
                                 <div className="flex flex-col items-center justify-center w-3/4 pb-5 mx-auto my-auto mt-5 lg:hidden ">
                                     <h2 className="text-3xl font-bold text-[#2cb74c] w-60 text-start">fun quizzes</h2>
-                                    <h3 className={`my-1 text-base md:text-xl font-bold  ${darkMode ? 'text-gray-100' : 'text-gray-950 w-60'}`}>
+                                    <h3 className={`my-1 text-base md:text-xl font-bold  ${darkMode ? 'text-gray-500' : 'text-gray-950 w-60'}`}>
                                         complete quizzes, earn XP points, and increase your daily streak
                                     </h3>
                                 </div>
@@ -348,10 +348,10 @@ const StartPage = () => {
                                     <div key={index} className="flex items-center">
                                         <button className="relative p-2.5 border rounded-md whitespace-nowrap text-gray-950">
                                             <span
-                                                className={`absolute inset-0 rounded-md ${darkMode ? 'opacity-60' : 'opacity-20'}  `}
+                                                className={`absolute inset-0 rounded-md ${darkMode ? 'opacity-30' : 'opacity-20'}  `}
                                                 style={{ backgroundColor: skill.color }}
                                             ></span>
-                                            <span className={`relative  font-semibold ${darkMode ? ' text-gray-100' : 'text-gray-950'}`}>{skill.skill}</span>
+                                            <span className={`relative  font-semibold ${darkMode ? ' text-gray-950' : 'text-gray-950'}`}>{skill.skill}</span>
                                         </button>
                                     </div>
                                 ))}
@@ -362,7 +362,7 @@ const StartPage = () => {
                     <Reveal motionType={'zoom'}>
                         <div className="flex items-center justify-center pt-20 md:pb-7">
                             <h1 className="text-2xl font-bold lg:text-4xl">
-                                <span className="text-[#BF5700] font-mono">WARNING:</span> <span className={`${darkMode ? 'text-gray-100' : 'text-gray-950'}`}>You will hate</span>  <span className="text-[#2cb74c]" style={{ letterSpacing: '1.5px' }}>fingo</span> <span className={`${darkMode ? 'text-gray-100' : 'text-gray-950'}`}>if...</span>
+                                <span className="text-[#BF5700] font-mono">WARNING:</span> <span className={`${darkMode ? 'text-gray-500' : 'text-gray-950'}`}>You will hate</span>  <span className="text-[#2cb74c]" style={{ letterSpacing: '1.5px' }}>fingo</span> <span className={`${darkMode ? 'text-gray-500' : 'text-gray-950'}`}>if...</span>
                             </h1>
                         </div>
                         <div style={{ height: '450px', overflowX: 'scroll', overflowY: 'hidden', display: 'flex' }}>
