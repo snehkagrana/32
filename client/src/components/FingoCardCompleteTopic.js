111fd4c43af1d4eec44b5bb5b16b529a70d03462
hiriski <hiriski@outlook.com>
2023-12-09 11:17:37 +0700

    fix: newUser state

--- client/src/components/FingoCardCompleteTopic.js
+++ client/src/components/FingoCardCompleteTopic.js
@@ -1,15 +1,10 @@
-import { useRef, useState } from 'react'
-import { useDispatch } from 'react-redux'
 import { useApp, useAuth } from 'src/hooks'
 import { Card } from 'react-bootstrap'
 import 'src/styles/FingoCardCompleteTopic.styles.css'
 
 const FingoCardCompleteTopic = () => {
-    const dispatch = useDispatch()
     const { skills } = useApp()
-    const { user } = useAuth()
-    const role = useRef('')
-    const [newUser, setNewUser] = useState(false)
+    const { user, newUser } = useAuth()
 
     const storedScores = newUser
         ? JSON.parse(sessionStorage.getItem('scores')) || []
