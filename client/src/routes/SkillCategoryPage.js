1ebda3d0a2fd2a09069a3d9bbc45debe6d75963e
hiriski <hiriski@outlook.com>
2023-12-10 21:58:36 +0700

    fix: sticky back to top, streak card dark mode, icon sub category

--- client/src/routes/SkillCategoryPage.js
+++ client/src/routes/SkillCategoryPage.js
@@ -16,8 +16,6 @@ import Navbar from "../components/Navbar";
 import  "../index.css";
 import '../styles/SkillCategoryPage.styles.css'
 import { FingoHomeLayout } from "src/components/layouts";
-import CompleteIcon from 'src/assets/images/complete.png'
-import UncompleteIcon from 'src/assets/images/uncomplete.png'
 import FingoWidgetContainer from "src/components/FingoWidgetContainer";
 import { FingoScrollToTop } from "src/components/layouts/FingoHomeLayout";
 
@@ -138,7 +136,6 @@ const SkillCategoryPage = () => {
                             <Row className="justify-content-md-center">
                                 <Col>
                                     <div className="sub_category_card_container">
-                                        <FingoScrollToTop />
                                         <button className="back-arrow" onClick={handleClick}>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="currentColor" fill-rule="evenodd" d="M5.841 5.28a.75.75 0 0 0-1.06-1.06L1.53 7.47L1 8l.53.53l3.25 3.25a.75.75 0 0 0 1.061-1.06l-1.97-1.97H14.25a.75.75 0 0 0 0-1.5H3.871l1.97-1.97Z" clip-rule="evenodd"/></svg>
                                         </button>
@@ -159,13 +156,11 @@ const SkillCategoryPage = () => {
                                             {subCategories.map((sub_category, i) => (
                                                 <Col key={i}>
                                                     <div className="sub_category_card_item_container d-flex flex-column justify-center align-items-center">
-                                                        <div className="sub_category_chapter_icon_container" onClick={() =>  handleSubCategorySelection(sub_category.sub_category)}>
-                                                            <div className={`sub_category_chapter_icon-${checkIsCompleted.current.includes(
+                                                        <div className={`sub_category_chapter_icon_container ${checkIsCompleted.current.includes(
                                                                 sub_category.sub_category
-                                                            ) ? "complete": "incomplete"}`}>
-                                                                <img src={checkIsCompleted.current.includes(
-                                                                    sub_category.sub_category
-                                                                ) ? CompleteIcon : UncompleteIcon} alt="icon" />
+                                                            ) ? "complete": "incomplete"}`} onClick={() =>  handleSubCategorySelection(sub_category.sub_category)}>
+                                                            <div className={`sub_category_chapter_icon-`}>
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m9 20.42l-6.21-6.21l2.83-2.83L9 14.77l9.88-9.89l2.83 2.83L9 20.42Z"/></svg>
                                                             </div>
                                                             {/* <div className="sub_category_chapter_ic_circle" /> */}
                                                         </div>
@@ -197,6 +192,7 @@ const SkillCategoryPage = () => {
                                                 </Col>
                                             ))}
                                         </Row>
+                                        <FingoScrollToTop />
                                     </div>
                                 </Col>
                             </Row>
