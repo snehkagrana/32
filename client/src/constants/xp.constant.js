970809777358fc5473a71c5f925c54964548a9b0
hiriski <hiriski@outlook.com>
2023-12-13 17:04:12 +0700

    feat(xp level): add xp constants

---
+++ client/src/constants/xp.constant.js
@@ -0,0 +1,41 @@
+export const XP_LEVEL_COLORS_DEFAULT = [
+    '#FF9E00',
+    '#7dc009',
+    '#0192FC',
+    '#D20DF2',
+    '#F84E4E',
+    '#00DB76',
+    '#F326FF',
+    '#01C817',
+    '#B2FF02',
+    '#ff7805',
+    '#7D5FDB',
+]
+
+export const XP_LEVEL_COLORS_LIGHT = [
+    '#FFF3CC',
+    '#F1FBCB',
+    '#CCF4FE',
+    '#FECEF5',
+    '#FEE7DB',
+    '#CAFDD1',
+    '#FFDCF5',
+    '#D6FCCA',
+    '#F5FFCC',
+    '#FFEECD',
+    '#EFE6FF',
+]
+
+export const XP_LEVEL_COLORS_DARK = [
+    '#B76300',
+    '#4F8A04',
+    '#0071D8',
+    '#A409D0',
+    '#B2273F',
+    '#009D74',
+    '#9728B7',
+    '#00902A',
+    '#74B701',
+    '#B74202',
+    '#5A42B7',
+]
