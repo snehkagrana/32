1fa0cfd30965c12770b65eae91a4922821f219ae
hiriski <hiriski@outlook.com>
2023-12-17 07:38:33 +0700

    feat(rewards): add ui redeem success

--- client/src/components/reward/ModalRewardDetail.jsx
+++ client/src/components/reward/ModalRewardDetail.jsx
@@ -1,45 +1,56 @@
-import React, { useCallback, useMemo } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import { useDispatch } from 'react-redux'
-import { useReward } from 'src/hooks'
+import { useAuth, useReward } from 'src/hooks'
 import { FingoButton, FingoModal } from 'src/components/core'
-import * as Yup from 'yup'
 import AmazonImg from 'src/assets/images/giftcard/amazon.jpg'
 import GooglePlayImg from 'src/assets/images/giftcard/google-play.jpg'
 import OtherImg from 'src/assets/images/giftcard/other.jpg'
 import 'src/styles/ModalRewardDetail.styles.css'
 import { RewardApi } from 'src/api'
-import Swal from 'sweetalert2'
+import LoadingBox from '../LoadingBox'
 
 const ModalRewardDetail = () => {
     const dispatch = useDispatch()
+    const { auth_syncAndGetUser } = useAuth()
     const { modalDetail, reward_setModalDetail } = useReward()
+    const [redeemSuccess, setRedeemSuccess] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+    const [redeemedItem, setRedeemedItem] = useState(null)
 
     const handleCloseModal = () => {
         dispatch(reward_setModalDetail(false))
     }
 
     const onClickRedeem = useCallback(async () => {
+        setIsLoading(true)
         try {
             const response = await RewardApi.redeem({
                 itemId: modalDetail.data._id,
                 notes: '',
             })
             if (response) {
-                Swal.fire({
-                    title: 'Success',
-                    text: 'Redeem successfully!',
-                    icon: 'success',
-                    showCancelButton: false,
-                    confirmButtonColor: '#009c4e',
-                    confirmButtonText: 'Ok',
-                }).then(result => {
-                    if (result.isConfirmed) {
-                        handleCloseModal()
+                setRedeemSuccess(true)
+                auth_syncAndGetUser().then(result => {
+                    setIsLoading(true)
+                    if (result?._id) {
+                        if (result?.rewards?.length > 0) {
+                            setIsLoading(false)
+
+                            setRedeemedItem(
+                                result?.rewards.find(
+                                    x => x._id === modalDetail.data._id
+                                )
+                            )
+                        }
+                    } else {
+                        setIsLoading(true)
                     }
                 })
             }
-        } catch (e) {}
+        } catch (e) {
+            setIsLoading(true)
+        }
     }, [modalDetail.data])
 
     const getGiftCardImage = useMemo(() => {
@@ -63,6 +74,14 @@ const ModalRewardDetail = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [modalDetail.data])
 
+    useEffect(() => {
+        if (!modalDetail.open) {
+            setIsLoading(false)
+            setRedeemSuccess(false)
+            setRedeemedItem(null)
+        }
+    }, [modalDetail.open])
+
     return (
         <FingoModal
             open={modalDetail.open}
@@ -70,70 +89,113 @@ const ModalRewardDetail = () => {
             centered
             className='ModalRewardDetail'
         >
-            {modalDetail.data && (
-                <div className='RewardDetail FingoShapeRadius'>
-                    <div className='FingoWaves'>
-                        <div />
-                        <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            viewBox='0 0 1440 320'
-                        >
-                            <path
-                                fill='currentColor'
-                                fillOpacity='1'
-                                d='M0,128L60,122.7C120,117,240,107,360,112C480,117,600,139,720,160C840,181,960,203,1080,192C1200,181,1320,139,1380,117.3L1440,96L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z'
-                            ></path>
-                        </svg>
-                    </div>
-                    <div className='HeaderTitle'>
-                        <h2 className='mb-3'>{modalDetail.data.name}</h2>
-                    </div>
-                    <div className='RewardDetailContent'>
-                        <div className='RewardDetailImg FingoShapeRadius'>
-                            <img
-                                src={getGiftCardImage}
-                                alt={modalDetail.data?.name}
-                            />
-                        </div>
-                        <div>
-                            <Row className='align-items-center justify-space-between'>
-                                <Col xs={12} className='mb-3'>
-                                    <h6 className='mb-1'></h6>
-                                    <p className='mb-0'>
-                                        Redeem for 💎{' '}
-                                        {modalDetail.data.diamondValue}
-                                    </p>
-                                    <hr />
-                                </Col>
-                                <Col xs={12} className='mb-3'>
-                                    <h6 className='mb-1'>Description</h6>
-                                    <p className='mb-0'>
-                                        {modalDetail.data.description ?? '-'}
-                                    </p>
-                                    <hr />
-                                </Col>
-                                <Col xs={12} className='mb-3'>
-                                    <h6 className='mb-1'>URL</h6>
-                                    <p className='mb-0'>
-                                        {modalDetail.data.brandUrl ?? '-'}
-                                    </p>
-                                    <hr />
-                                </Col>
-                                <Col xs={12}>
-                                    <FingoButton
-                                        size='lg'
-                                        color='success'
-                                        onClick={onClickRedeem}
-                                        style={{ width: '100%' }}
+            <div className='RewardDetail FingoShapeRadius'>
+                {isLoading ? (
+                    <LoadingBox height={300} />
+                ) : (
+                    <>
+                        {modalDetail.data && (
+                            <>
+                                <div className='FingoWaves'>
+                                    <div />
+                                    <svg
+                                        xmlns='http://www.w3.org/2000/svg'
+                                        viewBox='0 0 1440 320'
                                     >
-                                        Redeem
-                                    </FingoButton>
-                                </Col>
-                            </Row>
-                        </div>
-                    </div>
-                </div>
-            )}
+                                        <path
+                                            fill='currentColor'
+                                            fillOpacity='1'
+                                            d='M0,128L60,122.7C120,117,240,107,360,112C480,117,600,139,720,160C840,181,960,203,1080,192C1200,181,1320,139,1380,117.3L1440,96L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z'
+                                        ></path>
+                                    </svg>
+                                </div>
+                                <div className='HeaderTitle'>
+                                    <h2 className='mb-3'>
+                                        {redeemSuccess && redeemedItem
+                                            ? 'Congratulations!'
+                                            : modalDetail.data.name}
+                                    </h2>
+                                </div>
+                                <div className='RewardDetailContent'>
+                                    <div className='RewardDetailImg FingoShapeRadius'>
+                                        <img
+                                            src={getGiftCardImage}
+                                            alt={modalDetail.data?.name}
+                                        />
+                                    </div>
+
+                                    {redeemSuccess && redeemedItem ? (
+                                        <div className='RedeemSuccess'>
+                                            <h2>
+                                                Here's is the code for your{' '}
+                                                {redeemedItem?.currencyValue}{' '}
+                                                {redeemedItem?.currencyCode}{' '}
+                                            </h2>
+                                            <h6 className='mb-4'>
+                                                {redeemedItem?.name}
+                                            </h6>
+
+                                            <div className='RedeemCode'>
+                                                <code>
+                                                    {redeemedItem?.claimCode}
+                                                </code>
+                                            </div>
+                                        </div>
+                                    ) : (
+                                        <div>
+                                            <Row className='align-items-center justify-space-between'>
+                                                <Col xs={12} className='mb-3'>
+                                                    <h6 className='mb-1'></h6>
+                                                    <p className='mb-0'>
+                                                        Redeem for 💎{' '}
+                                                        {
+                                                            modalDetail.data
+                                                                .diamondValue
+                                                        }
+                                                    </p>
+                                                    <hr />
+                                                </Col>
+                                                <Col xs={12} className='mb-3'>
+                                                    <h6 className='mb-1'>
+                                                        Description
+                                                    </h6>
+                                                    <p className='mb-0'>
+                                                        {modalDetail.data
+                                                            .description ?? '-'}
+                                                    </p>
+                                                    <hr />
+                                                </Col>
+                                                <Col xs={12} className='mb-3'>
+                                                    <h6 className='mb-1'>
+                                                        URL
+                                                    </h6>
+                                                    <p className='mb-0'>
+                                                        {modalDetail.data
+                                                            .brandUrl ?? '-'}
+                                                    </p>
+                                                    <hr />
+                                                </Col>
+                                                <Col xs={12}>
+                                                    <FingoButton
+                                                        size='lg'
+                                                        color='success'
+                                                        onClick={onClickRedeem}
+                                                        style={{
+                                                            width: '100%',
+                                                        }}
+                                                    >
+                                                        Redeem
+                                                    </FingoButton>
+                                                </Col>
+                                            </Row>
+                                        </div>
+                                    )}
+                                </div>
+                            </>
+                        )}
+                    </>
+                )}
+            </div>
         </FingoModal>
     )
 }
