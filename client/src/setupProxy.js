82dc4443e7041052f25af7c1a0ac3f746cfaf0b0
hiriski <hiriski@outlook.com>
2023-12-17 18:25:01 +0700

    chore: dynamic proxy port for macos

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,15 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware')
+const os = require('os')
+
+// Port 5001 for macos
+const PORT = os.platform() === 'darwin' ? 5001 : 5000
 
 module.exports = function (app) {
     app.use(
         '/server',
         createProxyMiddleware({
-            target: 'http://localhost:5000',
+            target: `http://localhost:${PORT}`,
             changeOrigin: true,
         })
-    );
-};
+    )
+}
