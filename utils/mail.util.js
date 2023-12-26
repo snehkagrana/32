a1e3d0e99da1bd505eb2473ccb5d97cc45d3ce61
hiriski <hiriski@outlook.com>
2023-12-26 23:45:57 +0700

    chore: revert development email config

--- utils/mail.util.js
+++ utils/mail.util.js
@@ -1,14 +1,14 @@
 const nodemailer = require('nodemailer')
 
 const mailTransporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
+    host: 'smtpout.secureserver.net',
     secure: true,
     secureConnection: false, // TLS requires secureConnection to be false
     tls: {
         ciphers: 'SSLv3',
     },
     requireTLS: true,
-    port: process.env.MAIL_PORT,
+    port: 465,
     debug: true,
     auth: {
         user: process.env.MAIL,
