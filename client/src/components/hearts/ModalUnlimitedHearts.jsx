687f17158807cf64e8132255f1083cd439baa571
hiriski <hiriski@outlook.com>
2023-12-31 17:43:45 +0700

    feat: add sign up button for user non-logged in

--- client/src/components/hearts/ModalUnlimitedHearts.jsx
+++ client/src/components/hearts/ModalUnlimitedHearts.jsx
@@ -2,14 +2,16 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import { useApp } from 'src/hooks'
-import { FingoModal } from 'src/components/core'
+import { useApp, useAuth } from 'src/hooks'
+import { FingoButton, FingoModal } from 'src/components/core'
 import { ReactComponent as UnlimitedHeartIcon } from 'src/assets/svg/unlimited-hearts.svg'
 import RockerImage from 'src/assets/images/rocket.png'
 import 'src/styles/ModalUnlimitedHearts.styles.css'
 
 const ModalUnlimitedHearts = () => {
     const dispatch = useDispatch()
+
+    const { isAuthenticated, auth_setOpenModalRegister } = useAuth()
 
     const {
         openModalUnlimitedHearts,
@@ -23,6 +25,10 @@ const ModalUnlimitedHearts = () => {
 
     const onClickGetUnlimitedHears = () => {
         dispatch(app_setOpenModalInviteFriends(true))
+    }
+
+    const onClickSignUp = () => {
+        dispatch(auth_setOpenModalRegister(true))
     }
 
     return (
@@ -43,16 +49,36 @@ const ModalUnlimitedHearts = () => {
                         </h4>
                     </div>
                     <div className='UnlimitedHeartsContent'>
-                        <button
-                            className='UnlimitedHeartsBtn mb-3'
-                            onClick={onClickGetUnlimitedHears}
-                        >
-                            <UnlimitedHeartIcon />
-                            <span>Get Unlimited Hearts</span>
-                        </button>
-                        <p className='mb-0 text-center text-sm'>
-                            Get 72 hours of unlimited hearts by invite friends
-                        </p>
+                        {isAuthenticated ? (
+                            <>
+                                <button
+                                    className='UnlimitedHeartsBtn mb-3'
+                                    onClick={onClickGetUnlimitedHears}
+                                    disabled={!isAuthenticated}
+                                >
+                                    <UnlimitedHeartIcon />
+                                    <span>Get Unlimited Hearts</span>
+                                </button>
+                                <p className='mb-0 text-center text-sm'>
+                                    Get 72 hours of unlimited hearts by invite
+                                    friends
+                                </p>
+                            </>
+                        ) : (
+                            <>
+                                <FingoButton
+                                    onClick={onClickSignUp}
+                                    enableHoverEffect={false}
+                                    className='w-100'
+                                    color='white'
+                                >
+                                    Sign Up
+                                </FingoButton>
+                                <p className='mt-3 mb-0 text-center text-sm'>
+                                    Sign up to get unlimited hearts{' '}
+                                </p>
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
