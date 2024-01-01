5b167b6dbb6b0fb97aad477110b9a9ce899b3769
hiriski <hiriski@outlook.com>
2024-01-01 15:07:52 +0700

    fix: unlimited hearts modal not closing on clicking button

--- client/src/components/hearts/ModalUnlimitedHearts.jsx
+++ client/src/components/hearts/ModalUnlimitedHearts.jsx
@@ -24,6 +24,7 @@ const ModalUnlimitedHearts = () => {
     }
 
     const onClickGetUnlimitedHears = () => {
+        handleCloseModal()
         dispatch(app_setOpenModalInviteFriends(true))
     }
 
