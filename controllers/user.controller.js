f6484d12307248a8659b57fec3784e67c1fdbf16
hiriski <hiriski@outlook.com>
2024-02-05 23:39:23 +0700

    feat(account): add api get qr code user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,7 @@
+const { appConfig } = require('../configs/app.config')
 const UserService = require('../services/user.service')
+const UserModel = require('../models/user')
+const QRCode = require('qrcode')
 
 exports.findAll = async (req, res) => {
     const result = await UserService.findAll(req.params)
@@ -9,4 +12,18 @@ exports.findAll = async (req, res) => {
         })
     }
     return res.status(400).json({ message: 'Failed to get users' })
+}
+
+exports.getUserQr = async (req, res) => {
+    const user = await UserModel.findById(req.params.id)
+    if (user) {
+        const userLink = `${appConfig.appBaseUrl}/profile/${user.username}`
+        QRCode.toDataURL(userLink, function (err, url) {
+            return res.json({
+                qr: url,
+            })
+        })
+    } else {
+        return res.status(400).json({ message: 'Failed to get QR code user' })
+    }
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,6 +5,9 @@ const UserController = require('../controllers/user.controller')
 const ErrorHandler = require('../middlewares/error.middleware')
 const AuthGuard = require('../middlewares/auth.middleware')
 
+// update profile
+router.get('/user/qr/:id', ErrorHandler(UserController.getUserQr))
+
 // Get users
 router.get('/user', AuthGuard, ErrorHandler(UserController.findAll))
 
