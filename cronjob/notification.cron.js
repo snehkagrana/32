500a8a37da43bf142d71f0e86076bfe060418b73
hiriski <hiriski@outlook.com>
2024-04-23 05:51:52 +0700

    feat(notification): add cron notification

---
+++ cronjob/notification.cron.js
@@ -0,0 +1,27 @@
+const cron = require('node-cron')
+const UserModel = require('../models/user')
+const { NOTIFICATION_TYPE } = require('../constants/app.constant')
+
+const NotificationService = require('../services/notification.service')
+
+cron.schedule('*/10 * * * *', async function () {
+    const today = new Date()
+    const users = await UserModel.find({
+        fcmToken: { $exists: true },
+    }).exec()
+
+    console.log('CRONJOB notification running')
+
+    if (users.length > 0) {
+        users.forEach(async user => {
+            const notificationData = {
+                title: `Hi ${user.displayName || user.username}`,
+                body: `This is a notification schedule that sent every 10 minutes to user has FCM token`,
+                userId: user._id,
+                type: NOTIFICATION_TYPE.common,
+                dataId: null,
+            }
+            await NotificationService.sendAndSaveNotification(notificationData)
+        })
+    }
+})
