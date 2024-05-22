f8f3ece6d056d08d6f6de9728f5057c1215b5995
hiriski <hiriski@outlook.com>
2024-05-22 11:27:27 +0700

    fix(notificaiton): fix undefined imageUrl

--- models/notification.js
+++ models/notification.js
@@ -4,6 +4,7 @@ const NotificationModel = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     title: { type: String, required: true },
     body: { type: String, required: true },
+    imageUrl: { type: String },
     type: {
         type: String,
         enum: [
